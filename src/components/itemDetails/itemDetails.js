de9782089120f71310eb37ede0e5bffc536c514d
shreyas <shreyasbhat2001@gmail.com>
2020-07-08 17:39:44 +0530

    first commit

---
+++ src/components/itemDetails/itemDetails.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Row, Col, Button, Collapse, Well, Media } from 'react-bootstrap';
+
+export default class ItemDetails extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            open: false
+        };
+    }
+    render(){
+        return(
+            <div>
+                <Button>
+                    
+                </Button>
+            </div>
+
+        )
+    }
+}
